67f9e8c5b948aab77359eeb7c586bc9270747b77
yulikusch <36503003+yulikusch@users.noreply.github.com>
2022-12-29 19:39:51 +0700

    Add files via upload

---
+++ storage/framework/views/1f0b6cb16e44df744c5bbb52ed8779a4a1216b0b.php
@@ -0,0 +1,12 @@
+<div>
+    <header id="header" class="fixed-top">
+        <div class="container d-flex align-items-center">
+            <h1>Casscom</h1>
+            <nav id="navbar" class="navbar order-last order-lg-0">
+                <ul>
+                    <li>home</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</div><?php /**PATH D:\project laravel + livewire\casscom-livewire\resources\views/livewire/counter.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/fcdbceb1de7cad14db7d832297f377db38f9a081.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/app.css">
+    <?php echo \Livewire\Livewire::styles(); ?>
+
+</head>
+
+<body>
+    <?php
+if (! isset($_instance)) {
+    $html = \Livewire\Livewire::mount('counter', [])->html();
+} elseif ($_instance->childHasBeenRendered('0pAV2tj')) {
+    $componentId = $_instance->getRenderedChildComponentId('0pAV2tj');
+    $componentTag = $_instance->getRenderedChildComponentTagName('0pAV2tj');
+    $html = \Livewire\Livewire::dummyMount($componentId, $componentTag);
+    $_instance->preserveRenderedChild('0pAV2tj');
+} else {
+    $response = \Livewire\Livewire::mount('counter', []);
+    $html = $response->html();
+    $_instance->logRenderedChild('0pAV2tj', $response->id(), \Livewire\Livewire::getRootElementTagName($html));
+}
+echo $html;
+?>
+    <?php echo \Livewire\Livewire::scripts(); ?>
+
+</body>
+
+</html><?php /**PATH D:\project laravel + livewire\casscom-livewire\resources\views/home.blade.php ENDPATH**/ ?>
